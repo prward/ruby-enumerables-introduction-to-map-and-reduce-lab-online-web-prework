91787b1a13690479010bf56ad65bb01c39083c7f
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 17:45:58 +0000

    Automatically backed up by Learn

--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'my own map-like methods' do
   describe "map_to_negativize returns an array with all values made negative" do
     it "transforms correctly" do
-      expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, 9])
+      expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, -9])
     end
   end
 
