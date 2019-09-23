c966912f980621bca9781fbaa9d78712438c3ed9
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 18:38:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,42 @@
 # My Code here....
+def map_to_negativize (source_array)
+    source_array.map do |n|
+      if n > 0 
+    -n 
+  else
+    n 
+  end
+ end
+end
+
+def map_to_no_change (source_array)
+  ["paul", "gurney", "vladimir", "jessica", "chani"]
+end 
+
+def map_to_double (source_array)
+  source_array.map do |n|
+    n *2
+  end
+end
+  
+def map_to_square (source_array)  
+  source_array.map do |n|
+    n**2
+  end
+end
+  
+def reduce_to_total(source_array, starting_point = 0)
+    source_array.reduce(starting_point) { |sum, num| sum + num}
+end
+
+def reduce_to_all_true(source_array)
+    source_array.all?
+end  
+  
+def reduce_to_any_true(source_array)
+    source_array.any?
+  end
+  
+  
+  
+  
