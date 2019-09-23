8a8341fc8ef536e215d12df0722168d02c06c014
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 17:40:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 # My Code here....
 def map_to_negativize(source_array)
   Array.map.with_index do |x, i|
-    (source_array[i+1].nil? || x > array[i+1]) ? x : x*-1
+    (Array[i+1].nil? || x > array[i+1]) ? x : x*-1
   end
 end
