5a2976b0cf152bbb3e019ee97bba2ba68bf95fd9
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 17:43:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,3 @@
 # My Code here....
-def map_to_negativize(source_array)
-    array.each do |x|
-   answer << if x > 0
-       x * -1
-   else
-       x 
-   end
- end
+def map_to_negativize = []
+    
