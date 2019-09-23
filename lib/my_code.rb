a7a22c4110889a68ce36192819e20071b236d73f
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 18:28:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,9 +25,9 @@ def map_to_square (source_array)
   end
 end
   
-def reduce_to_total (source_array)  
-  source_array.reduce (:+)
-  end
+def reduce_to_total(source_array, starting_point = 0)
+    source_array.reduce(starting_point) { |sum, num| sum + num}
+end
 
 
 
