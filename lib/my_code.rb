90ea4f1eafca21c5a747d9e65f2cffa7ca615476
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 18:16:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,7 +30,8 @@ def reduce_to_total (source_array)
   end
 
 def reduce_to_total (source_array, starting_point = 100)
-  source_array.reduce
+  source_array.reduce(starting_point) (:+)
+end
   
   
   
